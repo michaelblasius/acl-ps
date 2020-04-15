a4334cad07f0f12de0d15a32159e9d6dc0dfe03c
Michael Blasius <63729862+michaelblasius@users.noreply.github.com>
2020-04-15 11:54:18 -0500

    changed format and added options
    Changed format of the query and reporting and added the option of Network Share or Local Folder and changed the process accordingly

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Access Control List PowerShell Script
   - https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item?view=powershell-7
   - https://stackoverflow.com/questions/303045/connecting-to-a-network-folder-with-username-password-in-powershell
   - https://www.youtube.com/watch?reload=9&v=3Ra4Y2Id8YI
+  - https://www.business.com/articles/powershell-interactive-menu/
--- acl.ps1
+++ acl.ps1
@@ -1,22 +1,46 @@
 ﻿#Access Control List PowerShell Script
-#20200414.01
-$DomainName = Read-Host -Prompt "domain" #domain name
-$UserName = Read-Host -Prompt "username" #user with rights
-$OpenDrive = Read-Host -Prompt "open drive letter (letter only)" #drive letter only that is available in system
-$RootPath = Read-Host -Prompt "network path (\\server\share)"  #define path to the shared folder
-New-PSDrive -Name $OpenDrive -PSProvider FileSystem -Root $path -Credential $DomainName\$UserName
-$OutFile = "C:\Permissions.csv"
-$Header = "Folder Path,IdentityReference,AccessControlType,IsInherited,InheritanceFlags,PropagationFlags"
-Del $OutFile
-New-Item -Path $OutFile
-Add-Content -Value $Header -Path $OutFile 
-$Folders = dir $RootPath -recurse | where {$_.psiscontainer -eq $true}
-
-foreach ($Folder in $Folders){
-	$ACLs = get-acl $Folder.fullname | ForEach-Object { $_.Access  }
-	Foreach ($ACL in $ACLs){
-	$OutInfo = $Folder.Fullname + "," + $ACL.IdentityReference  + "," + $ACL.AccessControlType + "," + $ACL.IsInherited + "," + $ACL.InheritanceFlags + "," + $ACL.PropagationFlags
-	Add-Content -Value $OutInfo -Path $OutFile
-	}}
-Remove-PSDrive $OpenDrive
-echo "saving to $OutFile"
+#20200415.02
+function Show-Menu
+{
+     param (
+           [string]$Title = 'Location'
+     )
+     cls
+     Write-Host "================ $Title ================"
+    
+     Write-Host "1: Press '1' Network Share."
+     Write-Host "2: Press '2' Local Drive."
+     Write-Host "Q: Press 'Q' to quit."
+}
+do
+{
+     Show-Menu
+     $input = Read-Host "Please make a selection"
+     switch ($input)
+     {
+           '1' {
+                cls
+				$DomainName = Read-Host -Prompt "domain" #domain name
+				$UserName = Read-Host -Prompt "username" #user with rights
+				$OpenDrive = Read-Host -Prompt "open drive letter (letter only)" #drive letter only that is available in system
+				$path = Read-Host -Prompt "network path"  #define path to the shared folder
+				$reportpath ="C:\acl.csv" #define path to export permissions report
+				New-PSDrive -Name $OpenDrive -PSProvider FileSystem -Root $path -Credential $DomainName\$UserName
+				dir -Recurse $path | where { $_.PsIsContainer } | % { $path1 = $_.fullname; Get-Acl $_.Fullname | % { $_.access | Add-Member -MemberType NoteProperty '.\Application Data' -Value $path1 -passthru }} | Export-Csv $reportpath
+				Remove-PSDrive $OpenDrive
+				echo "saving to $reportpath"
+				exit
+           } '2' {
+                cls
+				$path = Read-Host -Prompt "Folder location (ie c:\)"  #define path to the folder
+				$reportpath ="C:\acl.csv" #define path to export permissions report
+				dir -Recurse $path | where { $_.PsIsContainer } | % { $path1 = $_.fullname; Get-Acl $_.Fullname | % { $_.access | Add-Member -MemberType NoteProperty '.\Application Data' -Value $path1 -passthru }} | Export-Csv $reportpath
+				echo "saving to $reportpath"
+				exit
+           } 'q' {
+                return
+           }
+     }
+     pause
+}
+until ($input -eq 'q')
