82f2cfd287df0aa21909f1c3c95133714552011d
Michael Blasius <63729862+michaelblasius@users.noreply.github.com>
2020-04-15 09:43:48 -0500

    initial upload of script
    initial upload of script and credit added to readme

---
+++ acl.ps1
@@ -0,0 +1,20 @@
+﻿$DomainName = Read-Host -Prompt "domain" #domain name
+$UserName = Read-Host -Prompt "username" #user with rights
+$OpenDrive = Read-Host -Prompt "open drive letter (letter only)" #drive letter only that is available in system
+$RootPath = Read-Host -Prompt "network path (\\server\share)"  #define path to the shared folder
+New-PSDrive -Name $OpenDrive -PSProvider FileSystem -Root $path -Credential $DomainName\$UserName
+$OutFile = "C:\Permissions.csv"
+$Header = "Folder Path,IdentityReference,AccessControlType,IsInherited,InheritanceFlags,PropagationFlags"
+Del $OutFile
+New-Item -Path $OutFile
+Add-Content -Value $Header -Path $OutFile 
+$Folders = dir $RootPath -recurse | where {$_.psiscontainer -eq $true}
+
+foreach ($Folder in $Folders){
+	$ACLs = get-acl $Folder.fullname | ForEach-Object { $_.Access  }
+	Foreach ($ACL in $ACLs){
+	$OutInfo = $Folder.Fullname + "," + $ACL.IdentityReference  + "," + $ACL.AccessControlType + "," + $ACL.IsInherited + "," + $ACL.InheritanceFlags + "," + $ACL.PropagationFlags
+	Add-Content -Value $OutInfo -Path $OutFile
+	}}
+Remove-PSDrive $OpenDrive
+echo "saving to $OutFile"
